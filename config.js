3dfb728d6bb758a814b4ef8a65171834fc0d1123
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2021-05-17 09:57:12 +0530

    first HW

---
+++ config.js
@@ -0,0 +1,28 @@
+// Environment
+var environments = {};
+
+//staging environment
+environments.staging = {
+  httpPort: 3000,
+  httpsPort: 3001,
+  envName: "stading",
+};
+
+environments.production = {
+  httpPort: 5000,
+  httpsPort: 5001,
+  envName: "production",
+};
+
+// determine which env was passed as a commandline arugument
+var currentEnvironment =
+  typeof process.env.NODE_ENV == "string"
+    ? process.env.NODE_ENV.toLowerCase()
+    : "";
+
+var environmentToExport =
+  typeof environments[currentEnvironment] == "object"
+    ? environments[currentEnvironment]
+    : environments.staging;
+
+module.exports = environmentToExport;
